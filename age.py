29f1ca61a7004ebaaef5cd3b077d9b2e34df66a3
Michael Chary <mac389@gmail.com>
2014-03-21 15:57:49 -0400

    made approximation of p token = t

--- age.py
+++ age.py
@@ -1,21 +1,46 @@
-import nltk, os, json, csv
+import nltk, os, json, csv, string, cPickle
 from scipy.stats import scoreatpercentile
 
-#read the stopwrod file
-#do stopwords have to be in the same file as the json files?
+from pprint import pprint
+from progress.bar import Bar
 READ = 'rb'
-stopwords = open('../data/stopwords',READ).readlines()
+WRITE = 'wb'
+stopwords = set(open('stopwords',READ).read().splitlines())
+exclude = set(string.punctuation)
 
 #lemmatizer
-lmtzr = WordNetLemmatizer()
-
+lmtzr = nltk.stem.wordnet.WordNetLemmatizer()
+base = '/Volumes/My Book/carrie-controls/'
 #get the names of the files in a list
-json_list = os.listir('C:\Users\Carrie0731\Desktop\JSON FILES')#probably should modify this so everyone can open it on his/her computer
+json_list = [os.path.join(base,datafile) 
+			 for datafile in 
+			 filter(lambda string: string.endswith('json'),os.listdir(base))]
 
-sanitize = lambda text: lmtzer.lemmatize(text) if text not in stopwords else ''
-string = ' ' .join(map(sanitize,[tweet['text'] for tweet in json.load(open(filename,READ)) for filename in json_list]))
-
+def sanitize(wordList):
+	#Lemmatize
+	answer = [lmtzr.lemmatize(word.lower()) for word in list(set(wordList)-exclude)]
 	
-freq = FreqDist(string)
+	#Remove stopwords
+	answer = list(set(answer)-stopwords)
+
+	#Remove non-english words
+	answer = [word for word in answer if all([ord(letter)<128 for letter in word])]
+
+	#Remove links
+	answer = [word for word in answer if 'http://' not in word]
+	return answer
+
+words = []
+bar = Bar('Converting JSON files to proper text',max=len(json_list))
+for filename in json_list:
+	words.extend([sanitize(' '.join([tweet['text'] 
+			for tweet in json.load(open(filename,READ))]).split())] )
+	bar.next()
+bar.finish()
+
+words = [item for sublist in words for item in sublist]
+freq = nltk.FreqDist(words)
 cutoff = scoreatpercentile(freq.values(),15)
-vocab = [word for word,f in freq.items() if f > cutoff] #Items returns the tuple (key,value), in this case (word, frequency)
+vocab = [word for word,f in freq.items() if f > cutoff] 
+cPickle.dump({'distribution':freq,'cutoff':cutoff},open('freqdist.pkl',WRITE))
+#pprint(vocab)
