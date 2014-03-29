a2cf6b85677885d0798abafafbb65f046f03b176
Michael Chary <mac389@gmail.com>
2014-03-29 11:43:41 -0400

    update

--- calculate-age-probability.py
+++ calculate-age-probability.py
@@ -8,10 +8,15 @@ t_given_a = json.load(open('conditional_probability.json','rb'))
 a_unconditional = json.load(open('age.json','rb'))
 
 t_unconditional = cPickle.load(open('freqdist.pkl','rb'))['distribution']
-
-test_sentence = "i like frogs"
+denominator = sum(t_unconditional.values())
+test_sentence = "i like frogs nubs hit bob"
 tokens = word_tokenize(test_sentence)
 
+p = {}
 for token in tokens:
-	if all([token in [t_given_a,a_unconditional,t_unconditional]]):
-		pass
+	if all([token in [t_given_a,t_unconditional]]):
+		for age in a_unconditional:
+			p [age]= t_given_a[age][token]*float(a_unconditional[age])/(denominator*t_unconditional[word])
+
+#Doesn't account for bigrams
+#Need larger sample of unconditional token distribution
